904d2a5ed6b5824aebfbfff390e3d8a41594614e
Luis <luisinweb@gmail.com>
2015-10-09 07:28:35 -0300

    numero de versão do pacote atualizado

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'dinf:menu',
-  version: '1.0.0',
+  version: '1.1.0',
   // Brief, one-line summary of the package.
   summary: 'pacote contendo classes para criação de menus',
   // URL to the Git repository containing the source code for this package.
